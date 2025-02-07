45b51c93774e3a0cc40a20d4522067a1aeaf5215
anilkumarinjam <injamanilchowdary@gmail.com>
2025-02-07 00:54:26 -0600

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
