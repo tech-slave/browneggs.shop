94d1880826d1e1bb0dd1c9b12582f677462782f2
anilkumarinjam <injamanilchowdary@gmail.com>
2025-02-07 02:55:01 -0600

    base up

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function HomePage() {
 
 function App() {
   return (
-    <Router basename="/browneggs.shop">
+    <Router>
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
         <Navbar />
         <Routes>
