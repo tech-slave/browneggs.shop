2691b093c525686a8f65351ee4265e9c92cf1f02
anilkumarinjam <injamanilchowdary@gmail.com>
2025-02-08 22:53:36 -0600

    404 loop fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
