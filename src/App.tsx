f52619ba51013a55757e5a6147f06c8e2d8afa6b
anilkumarinjam <injamanilchowdary@gmail.com>
2025-02-12 00:06:49 -0600

    routing update

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function HomePage() {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
         <Navbar />
         <Routes>
